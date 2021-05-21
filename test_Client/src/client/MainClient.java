d852d9a80bcf0bf39d74aa7234dd5ef07626322a
CYOUJ <45324921+choiyoujin1201@users.noreply.github.com>
2021-05-21 18:03:04 +0900

    Video Size
    Not verified

--- test_Client/src/client/MainClient.java
+++ test_Client/src/client/MainClient.java
@@ -1,48 +1,49 @@
 package client;
 
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-
-import javax.swing.*;
-
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
 
 public class MainClient {
 	static Socket socket;
-	
-    public static void main(String[] args) throws IOException {
-    	
-    	Webcam webcam = Webcam.getDefault();
-    	webcam.open();
-    	
-    	socket = new Socket("127.0.0.1", 4000);
-    	
-    	BufferedImage bm = webcam.getImage();
-    	
-    	ObjectOutputStream dout = new ObjectOutputStream(socket.getOutputStream());
-    	
-    	ImageIcon im = new ImageIcon(bm);
-    	
-    	JFrame frame = new JFrame("PC 1");
-    	frame.setSize(640, 360);
-    	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-    	
-    	JLabel l = new JLabel();
-    	l.setVisible(true);
-    	
-    	frame.add(l);
-    	frame.setVisible(true);
-    	
-    	while (true) {
-    		bm = webcam.getImage();
-    		im = new ImageIcon(bm);
-    		dout.writeObject(im);
-    		l.setIcon(im);
-    		dout.flush();
-    	}
-    	
-    }
 
+	public static void main(String[] args) throws IOException{
+		
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		webcam.open(true);
+		
+		socket = new Socket("127.0.0.1", 5000);
+		
+		BufferedImage bm = webcam.getImage();
+		
+		ObjectOutputStream dout = new ObjectOutputStream(socket.getOutputStream());
+		
+		ImageIcon im = new ImageIcon(bm);
+		
+		JFrame frame = new JFrame("PC 1");
+		frame.setSize(700, 700);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		
+		JLabel l = new JLabel();
+		l.setVisible(true);
+		
+		frame.add(l);
+		frame.setVisible(true);
+		
+		while(true) {
+			bm = webcam.getImage();
+			im = new ImageIcon(bm);
+			dout.writeObject(im);
+			l.setIcon(im);
+			dout.flush();
+		}
+	}
 }
--- test_server/src/server/MainServer.java
+++ test_server/src/server/MainServer.java
@@ -4,49 +4,45 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MainServer {
 
-    public static void main(String[] args) {
-    	ServerSocket server;
+	public static void main(String[] args) {
+		ServerSocket server;
+		
 		try {
-			server = new ServerSocket(4000);
-	    	System.out.println("waiting..");
-	    	
-	    	Socket socket = server.accept();
-	    	System.out.println("connected..");
-	    	
-	    	ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-	    	
-	    	JLabel label = new JLabel();
-	    	JFrame frame = new JFrame();
-	    	frame.setSize(640, 360);
-	    	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-	    	
-	    	label = new JLabel();
-	    	label.setSize(640,360);
-	    	label.setVisible(true);
-	    	
-	    	frame.add(label);
-	    	frame.setVisible(true);
-	    	
-	    	while(true) {
-	    		try {
+			server = new ServerSocket(5000);
+			System.out.println("wating...");
+			
+			Socket socket = server.accept();
+			System.out.println("connected...");
+			
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			
+			JLabel label = new JLabel();
+			JFrame frame = new JFrame();
+			frame.setSize(700, 700);
+			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+			
+			label = new JLabel();
+			label.setSize(700, 700);
+			label.setVisible(true);
+			
+			frame.add(label);
+			frame.setVisible(true);
+			
+			while(true) {
+				try {
 					label.setIcon((ImageIcon)in.readObject());
 				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	    	}
-	    	
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    	
-    }
-
+	}
 }
