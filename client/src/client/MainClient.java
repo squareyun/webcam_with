6874dfd1e48baa3245a421050d175a1431078a6f
squareyun <squareyun@gmail.com>
2021-05-27 21:40:38 +0900

    refactoring client

--- client/src/client/MainClient.java
+++ client/src/client/MainClient.java
@@ -1,5 +1,70 @@
 package client;
 
-public class MainClient {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class MainClient {
+	Socket socket;
+	
+	public void startClient(String IP, int port) {
+		Thread thread = new Thread() {
+			public void run() {
+				try {
+					socket = new Socket(IP, port);
+					receive();
+				} catch (Exception e) {
+					stopClient();
+					System.out.println("[서버 접속 실패]");
+				}
+			}
+		};
+		thread.start();
+	}
+	
+	public void stopClient() {
+		try {
+			if(socket != null && !socket.isClosed()) {
+				socket.close();
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void receive() {
+		while(true) {
+			try {
+				InputStream in = socket.getInputStream();
+				byte[] buffer = new byte[512];
+				int length = in.read(buffer);
+				if(length == -1) throw new IOException();
+				String message = new String(buffer, 0, length, "UTF-8");
+			} catch (Exception e) {
+				stopClient();
+				break;
+			}
+		}
+	}
+	
+	public void send(String message) {
+		Thread thread = new Thread() {
+			public void run() {
+				try {
+					OutputStream out = socket.getOutputStream();
+					byte[] buffer = message.getBytes("UTF-8");
+					out.write(buffer);
+					out.flush();
+				} catch (Exception e) {
+					stopClient();
+				}
+			}
+		};
+		thread.start();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
