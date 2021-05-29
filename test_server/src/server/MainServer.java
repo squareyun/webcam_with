2a5caa43d0997ca657715997c2833294812db9a8
kes98514 <83210172+kes98514@users.noreply.github.com>
2021-05-29 19:39:03 +0900

    Update MainServer.java

--- test_server/src/server/MainServer.java
+++ test_server/src/server/MainServer.java
@@ -1,52 +1,140 @@
 package server;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+import java.awt.*;
+import javax.swing.JButton;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainServer {
+	public static ExecutorService threadPool;
+	public static Vector<Handler> users = new Vector<Handler>();
+	public static Webcam webcam;
+	public static JLabel l;
+	public static JFrame frame;
+	
+	TextField txt1 = new TextField("ì±„íŒ…");
+	TextField txt2 = new TextField("");
+	TextArea lbl = new TextArea("ì±„íŒ…ë‚´ì—­");
+	TextField txt3 = new TextField("");
+	
+	ServerSocket serverSocket;
 
-	public static void main(String[] args) {
-		ServerSocket server;
-		
+	public void startServer(String IP, int port) {
 		try {
-			server = new ServerSocket(5000);
-			System.out.println("wating...");
+			serverSocket = new ServerSocket(port);
 			
-			Socket socket = server.accept();
-			System.out.println("connected...");
-			
-			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-			
-			//frame°ú label »çÀÌÁî¸¦ 1000, 1000À¸·Î ÇØµÒ
-			//client ÄÚµå¿¡¼­ Ä· Å©±â Á¶Àı Å×½ºÆ®¸¦ À§ÇØ ÀÓÀÇ·Î ¹Ù²ãµĞ ºÎºĞ
-			//ÇÊ¿ä¿¡ µû¶ó Á¶ÀıÇÏ¿© »ç¿ë
-			JLabel label = new JLabel();
-			JFrame frame = new JFrame();
-			frame.setSize(1000, 1000);
-			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-			
-			label = new JLabel();
-			label.setSize(1000, 1000);
-			label.setVisible(true);
-			
-			frame.add(label);
-			frame.setVisible(true);
-			
-			while(true) {
-				try {
-					label.setIcon((ImageIcon)in.readObject());
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (!serverSocket.isClosed())
+				stopServer();
+			return;
+		}
+		
+		webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		webcam.open(true);
+		Webcam.getDiscoveryService().setEnabled(false);
+		Webcam.getDiscoveryService().stop();
+		frame = new JFrame("Server");
+		JButton btn4 = new JButton("ì „ì†¡");
+		JButton btn1 = new JButton("ë‚˜ê°€ê¸°");
+		JButton btn2 = new JButton("ë¬¸ì œë³€ê²½");
+		JButton btn3 = new JButton("??");
+		frame.setLayout(null);
+		lbl.setBounds(10,500,620,200); //ì±„íŒ…ë‚´ì—­
+		txt1.setBounds(10,710,520,40); //ì±„íŒ…ì¹˜ëŠ”ê³³
+		txt2.setBounds(670,70,200,410); //ì ìˆ˜íŒ
+		txt3.setBounds(720,20,100,40);
+		btn4.setBounds(530,710,100,40);
+		btn1.setBounds(720,700,100,40);
+		btn2.setBounds(720,630,100,40);
+		btn3.setBounds(720,560,100,40);
+		//í”„ë ˆì„ì— ì»´í¬ë„ŒíŠ¸ ì¶”ê°€
+		frame.add(lbl);
+		frame.add(txt1);
+		frame.add(txt2);
+		frame.add(txt3);
+		frame.add(btn4);
+		frame.add(btn1);
+		frame.add(btn2);
+		frame.add(btn3);
+		frame.setSize(700, 700);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		frame.pack();
+		l = new JLabel();
+		l.setSize(640,480);
+		l.setVisible(true);
+		
+		frame.add(l);
+		frame.setVisible(true);
+
+		// í´ë¼ë¦¬ì–¸íŠ¸ ì ‘ì† ëŒ€ê¸°
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Socket socket = serverSocket.accept();
+						users.add(new Handler(socket));
+						System.out.println("[í´ë¼ì´ì–¸íŠ¸ ì ‘ì†] " + socket.getRemoteSocketAddress() + ": "
+								+ Thread.currentThread().getName());
+					} catch (Exception e) {
+						if (!serverSocket.isClosed())
+							stopServer();
+						frame.dispose();
+						break;
+					}
 				}
 			}
-		} catch (IOException e) {
+		};
+		threadPool = Executors.newCachedThreadPool();
+		threadPool.submit(thread);
+	}
+
+	public void stopServer() {
+		try {
+			frame.dispose();
+			Iterator<Handler> it = users.iterator();
+			while (it.hasNext()) {
+				Handler client = it.next();
+				client.socket.close();
+				it.remove();
+			}
+
+			if (serverSocket != null && !threadPool.isShutdown())
+				threadPool.shutdown();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	public static void main(String[] args) {
+		MainServer m = new MainServer();
+		m.startServer("localhost", 55555);
+	}
+
 }
